b8bf26296e73f9bcaeb82a5f18b463263951766b
Sam <86075853+SamIp-ac@users.noreply.github.com>
2023-02-04 23:17:21 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title('learning steps')
-st.subheader('uu'）
+st.subheader('Progress')
 st.write('Heart Disease Detection [Git link](https://github.com/SamIp-ac/Code_Base_Period2_heartdisease.git)')
 st.write('bye')
