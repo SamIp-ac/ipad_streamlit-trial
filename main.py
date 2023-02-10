0bc78caffbe257c89e526d80e4f7b5ba58c1af1e
Sam <86075853+SamIp-ac@users.noreply.github.com>
2023-02-10 17:38:34 +0000

    meg

--- main.py
+++ main.py
@@ -7,8 +7,3 @@ st.markdown('Markdown: ')
 st.caption('caption:')
 st.write('Heart Disease Detection [Git link](https://github.com/SamIp-ac/Code_Base_Period2_heartdisease.git)')
 st.text('bye')
-
-"""
-with open( "style.css" ) as css:
-    st.markdown( f'<style>{css.read()}</style>' , unsafe_allow_html= True)
-"""
