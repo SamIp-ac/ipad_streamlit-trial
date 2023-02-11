3413dd643c0919c9603ff918535288d284e7d26f
Sam <86075853+SamIp-ac@users.noreply.github.com>
2023-02-12 02:57:27 +0800

    Update and rename pages/testing.py to Check_List.py

---
+++ Check_List.py
@@ -0,0 +1,97 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+st.write('success')
+
+st.header('Dev Tool')
+st.subheader('Communication:')
+tool1 = st.checkbox('
+OpenProject
+MatterMost')
+tools2 = st.checkbox('
+Deployment:
+Docker 
+Git — .gitignore
+GitHub
+
+App dev:
+Android studio — Flutter
+
+Web dev:
+VSCode — Flutter
+Django (Python)
+Flask
+
+Package:
+Anaconda
+
+Backend/Data science:
+Pycharm
+tfile Flutter
+
+Data analysis:
+RStudio
+Tableau
+Google analytics 
+D3.is (D3 chart python)
+
+API:
+Fastapi/Flask
+
+API testing:
+Postman
+
+API deploy:
+Heroku 
+VM (PVE — Proxmox — ubuntu)
+Google Cloud Run
+AWS gateways
+
+Database:
+Firebase
+MySQL
+Redis
+Multi-Cloud Storage:
+MinIO
+
+Animations:
+Unity
+Unreal Engine
+
+Terminal:
+iTerm 2
+Zsh
+Flutter doctor
+
+Version control:
+Flagsmith
+
+Data science web app:
+Streamlit — .streamlit
+
+Mail API:
+MailGun
+
+Web Scrapping:
+Beautiful Soap
+Scrappy
+URLLIB
+Selenium (harder)
+
+UI/UX:
+Adobe XD (To flutter)
+Figma (To flutter)
+
+App Marketing:
+Apple connect
+Google play console
+
+Web Services, System Development:
+Rust (Ruff)
+
+Big Data sources:
+https://www.finereport.com/tw/data-analysis/freedata.html
+')
