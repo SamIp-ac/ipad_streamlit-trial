6c207e1b4722e46ae61a4f08ced8b1cb1bc8395a
Sam <86075853+SamIp-ac@users.noreply.github.com>
2023-02-12 16:22:41 +0800

    Update Check_List.py

--- pages/Check_List.py
+++ pages/Check_List.py
@@ -8,7 +8,9 @@ st.write('success')
 
 st.header('Dev Tool')
 st.subheader('Communication:')
-tool1 = st.checkbox('OpenProject')
+
+tool1 = st.empty()
+tool1.checkbox('OpenProject')
 if tool1:
     st.checkbox('OpenProject', value=True)
     tool1 = st.empty()
