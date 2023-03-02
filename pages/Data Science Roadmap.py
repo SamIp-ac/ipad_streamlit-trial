12543f1cd6e0a4c887056b6ef30e4d82e783515c
Sam <86075853+SamIp-ac@users.noreply.github.com>
2023-03-02 19:18:35 +0800

    Update Data Science Roadmap.py

--- pages/Data Science Roadmap.py
+++ pages/Data Science Roadmap.py
@@ -2,5 +2,5 @@ from PIL import Image
 import os
 
 for file_ in os.listdir('Roadmap'):
-  image = Image.open('ipad_streamlit-trial/Roadmap/' + file_)
+  image = Image.open('Roadmap/' + file_)
   st.image(image, caption='Data_Science_Map')
